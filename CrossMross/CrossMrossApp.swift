639a1b8468883b3b6a94369ff42358c2960b0d1e
ahmetsenturk <iahmetsenturk@yahoo.com>
2023-12-22 20:52:27 +0100

    Initial Commit

---
+++ CrossMross/CrossMrossApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrossMrossApp.swift
+//  CrossMross
+//
+//  Created by Ahmet Senturk on 22.12.23.
+//
+
+import SwiftUI
+
+@main
+struct CrossMrossApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
